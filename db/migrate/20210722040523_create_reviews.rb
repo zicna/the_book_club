88ed52ab12bc3fae3ea148457994b753b347ecd3
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-07-21 23:06:02 -0500

    create Review resource

---
+++ db/migrate/20210722040523_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reviews do |t|
+      t.belongs_to :book, null: false, foreign_key: true
+      t.belongs_to :user, null: false, foreign_key: true
+      t.text :content
+      t.integer :mark
+
+      t.timestamps
+    end
+  end
+end
