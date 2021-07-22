49de800e53999833915d4e82b64c04fc2694f51b
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-07-21 22:48:47 -0500

    create Book resource

---
+++ db/migrate/20210722034809_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.belongs_to :author, null: false, foreign_key: true
+      t.belongs_to :category, null: false, foreign_key: true
+      t.string :title
+      t.string :invt
+      t.text :description
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
