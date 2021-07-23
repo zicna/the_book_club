848df5c1abcdd8220cf5b2573e52202453a3241a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-07-23 12:13:11 -0500

    working on usersController part 2.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-    before_action :get_user, only: [:show, :edit, :create, :destroy]
+    before_action :get_user, only: [:show, :edit, :update, :create, :destroy]
     before_action :set_user, only: :new
     before_action :get_users, only: :index
     
@@ -14,7 +14,6 @@ class UsersController < ApplicationController
     end
 
     def create
-        # byebug
         @user = User.new(user_params)
         if @user.save
             redirect_to user_path(@user), notice: "New User Created"
@@ -27,9 +26,19 @@ class UsersController < ApplicationController
     end
 
     def update
+        @user.update(user_params)
+        if @user.save
+            redirect_to user_path(@user), notice: "The user was successfully updated"
+        else
+            render :edit
+        end
+
     end
 
     def destroy
+        @user.destroy
+        flash[:alert] = "User has been deleted!"
+        redirect_to users_path
     end
 
     private
