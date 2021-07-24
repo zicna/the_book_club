bfa9588f8a927938d0d17ac25efeea93092dd3d9
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-07-24 00:19:04 -0500

    deleting an author raise this error 'ActiveRecord::InvalidForeignKey in AuthorsController#destroy'- trying to resolve it Part One.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ApplicationRecord
-    has_many :books
+    has_many :books, foreign_key: "author_id", dependent: :nullify
 
     def full_name
         "#{self.first_name} #{self.last_name}"
--- db/migrate/20210722034809_create_books.rb
+++ db/migrate/20210722034809_create_books.rb
@@ -1,7 +1,7 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-      t.belongs_to :author, null: false, foreign_key: true
+      t.belongs_to :author, foreign_key: true
       t.belongs_to :category, null: false, foreign_key: true
       t.string :title
       t.string :invt
