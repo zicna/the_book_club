f4a483d57980be64e3ca5871370822c1e1c37864
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-07-23 23:23:51 -0500

    create AuthorsController part 1.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,9 @@
 class AuthorsController < ApplicationController
-
+    
+    before_action :get_author, only: [:show, :edit, :update, :create, :destroy]
+    before_action :set_author, only: :new
+    before_action :get_authors, only: :index
+    
     def index
     end
 
@@ -10,17 +14,49 @@ class AuthorsController < ApplicationController
     end
 
     def create
+        @author = Author.new(author_params)
+        if @author.save
+            redirect_to author_path(@author), notice: "New author Created"
+        else
+            render :new
+        end
     end
 
     def edit
     end
 
     def update
+        @author.update(author_params)
+        if @author.save
+            redirect_to author_path(@author), notice: "The author was successfully updated"
+        else
+            render :edit
+        end
+
     end
 
     def destroy
+        @author.destroy
+        flash[:alert] = "Author has been deleted!"
+        redirect_to authors_path
     end
 
-    private 
+    private
+    def author_params
+        params.require(:author).permit(:first_name, :last_name, :image, :birth_date, :death_date, :wiki_page)
+    end
+
+    def get_author
+        @author = Author.find_by(id: params[:id])
+    end
+
+    def set_author
+        @author = Author.new
+    end
+
+    def get_authors
+        @authors = Author.all
+    end
+    
     
 end
