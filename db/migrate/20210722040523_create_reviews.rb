645f5510e3c6a1393a154af285254199db6be0de
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-07-25 15:34:36 -0500

    temp changes on user_id column, user_id, default => 1

--- db/migrate/20210722040523_create_reviews.rb
+++ db/migrate/20210722040523_create_reviews.rb
@@ -2,7 +2,8 @@ class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
       t.belongs_to :book, null: false, foreign_key: true
-      t.belongs_to :user, foreign_key: true
+      t.belongs_to :user, null: true, foreign_key: true, :default => 1
+      #Ex:- :default =>''
       t.text :content
       t.integer :mark
 
