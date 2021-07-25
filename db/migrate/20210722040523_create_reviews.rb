b34a0e9406937e1cd3fa3bad98b5d697fb9a4475
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-07-25 15:17:05 -0500

    allow review.user = nil by deleting 'null: false' on user_id column

--- db/migrate/20210722040523_create_reviews.rb
+++ db/migrate/20210722040523_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
       t.belongs_to :book, null: false, foreign_key: true
-      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :user, foreign_key: true
       t.text :content
       t.integer :mark
 
