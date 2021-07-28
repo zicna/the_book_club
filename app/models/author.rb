07aed51a4668458e11351fdf0dde82a24e7a96bf
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-07-27 19:55:52 -0500

    add validations for Author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,12 @@
 class Author < ApplicationRecord
     has_many :books, foreign_key: "author_id", dependent: :nullify
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :birth_date, presence :true, uniqueness: {
+        scope: [:first_name, :last_name]
+    }
+    
     
     def full_name
         "#{self.first_name} #{self.last_name}"
