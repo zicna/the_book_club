aeff2bae7ee8838bbba400bd9467622b05c7e7f4
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-07-28 01:01:02 -0500

    create Sessions Controller part one

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,28 @@
 class SessionsController < ApplicationController
+
+  def hello
+  end 
+
   def new
   end
 
   def create
+    
+    user = User.find_or_create_by(email: params[:user][:email], username: params[:user][:username]) do |u|
+      u.first_name = params[:user][:first_name],
+      u.last_name = params[:user][:last_name],
+      u.email = params[:user][:email],
+      u.username = params[:user][:username],
+      u.password = params[:user][:password],
+      u.birth_date = params[:user][:birth_date]
+    end
+    # byebug
+    if user.valid?
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      redirect_to login_path
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  
+  root "sessions#hello"
+  get '/login' => "sessions#new"
+  post '/login' => "sessions#create"
+  delete '/logout' => "sessions#destroy"
+
   resources :reviews
   resources :users
   resources :authors
   resources :categories
   resources :books
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
