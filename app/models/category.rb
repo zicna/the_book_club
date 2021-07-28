f697c58645b4ba525e4524402ceec5715b00a25a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-07-27 22:24:22 -0500

    add validations for Category, Review models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
     has_many :books
+
+    validates :name, presence: true, uniqueness: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,16 @@ class Review < ApplicationRecord
   belongs_to :book
   belongs_to :user
 
+  validates :book_id, presence: true
+  validates :user_id, presence: true, uniqueness: {
+    scope: :book_id}
+  validates :content, presence: true
+  validates :mark, numericality: {
+    only_integer: true,
+    :greater_than_or_equal_to => 1,
+    :less_than_or_equal_to => 10
+}
+
   def date_of_review
     self.created_at.strftime("%B %d, %Y")
   end
