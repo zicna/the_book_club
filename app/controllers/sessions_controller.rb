4d47c27e62a46681f6c24bb6b2ea8998e0317bd7
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-07-29 00:50:31 -0500

    create sessions#omniauth, small changes in views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,6 +28,30 @@ class SessionsController < ApplicationController
     end
   end
 
+  def omniauth #log user with google
+    # byebug
+    user = User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+      u.first_name = auth['info']['first_name']
+      u.last_name = auth['info']['last_name']
+      u.email = auth['info']['email']
+      u.password = SecureRandom.hex(10)
+      u.uid = auth['uid']
+      u.provider = auth['provider']
+    end
+    if user.valid?
+      flash[:message] = "You are logged in with google"
+      redirect_to authors_path
+    else
+      flash[:message] = user.errors.full_messages.join(", ")
+      redirect_to authors_path
+    end
+  end
+
   def destroy
+  end
+
+  private
+  def auth
+    request.env['omniauth.auth']
   end
 end
