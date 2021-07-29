1d33a888e03dc2e8d1a1ca4001b8dd062276519b
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-07-29 00:46:00 -0500

    changes in validations so we can create new user through sesions#omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,22 +7,22 @@ class User < ApplicationRecord
     #this gives us password and password_confirmation to our model
     #these fields do not correspond with database columns, method expect there to be password_digest column
 
-    validates :first_name, presence: true, uniqueness: {
-        scope: [:last_name, :birth_date]
-    }
+    # validates :first_name, presence: true, uniqueness: {
+    #     scope: [:last_name, :birth_date]
+    # }
+    validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :birth_date, presence: true
+    # validates :birth_date, presence: true
     validates :email, presence: true, uniqueness: true
 
-    validates :password, presence: true
-    validates :password_confirmation, presence: true
-    validates_confirmation_of :password
+    # validates :password, presence: true
+    # validates :password_confirmation, presence: true
+    # validates_confirmation_of :password
 
     # validates :username, presence: true, uniqueness: true
-    validates :birth_date, presence: true
 
 
     def full_name
-        "#{self.first_name} #{self.last_name}"
+        "#{self.first_name} #{self.last_name}" 
     end
 end
