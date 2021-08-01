8f927c5b746433315ee6d2f5403e775aaf77aa2c
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-08-01 16:05:25 -0500

    'one_review_per_user_per_book' helper rendering in books/show

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,8 @@
 module ReviewsHelper
+    #review is 'join' between users and books
+    #comb of one book and one user there can be only onew review
+    #this checks it and returns [] or ["something here"]
+    def one_review_per_user_per_book(book)
+        book.reviews.map{|review| review.user_id == current_user.id }
+    end
 end
