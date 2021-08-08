359f9c4dd7b5a809c64b6498d2bbc13757c7ac52
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-08-08 14:22:53 -0500

    add few more quotes for home page

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,4 +1,13 @@
 module WelcomeHelper
+
+    def book_quote
+        quote = BOOK_QUOTES[rand(1...BOOK_QUOTES.length)]
+        content_tag :div do 
+            content_tag :p, "'#{quote[:quote]}' by #{quote[:author]}", class:"quote"
+        end
+    end
+
+    private 
     BOOK_QUOTES = [
         {
             quote:"If you don’t like to read, you haven’t found the right book." ,
@@ -21,17 +30,38 @@ module WelcomeHelper
             author: "Marcus Tullius Cicero",
         },
         {
+            quote: "‘Classic’ – a book which people praise and don’t read." ,
+            author: "Marc Twain",
+        },
+        {
+            quote: "Think before you speak. Read before you think." ,
+            author: "Fran Lebowitz",
+        },
+        {
+            quote: "The reading of all good books is like conversation with the finest (people) of the past centuries." ,
+            author: "Descartes",
+        },
+        {
             quote: "There is no friend as loyal as a book" ,
             author: "Ernest Hemingway",
+        },
+        {
+            quote: "A good book is an event in my life." ,
+            author: "Stendal",
+        },
+        {
+            quote: "Make it a rule never to give a child a book you would not read yourself." ,
+            author: "George Bernard Shaaw",
+        },
+        {
+            quote: "To learn to read is to light a fire; every syllable that is spelled out is a spark." ,
+            author: "Victor Hugo",
+        },
+        {
+            quote: "Employ your time in improving yourself by other men’s writings so that you shall come easily by what others have labored hard for." ,
+            author: "Socrates",
         }
     ]
-
-    def book_quote
-        quote = BOOK_QUOTES[rand(1...BOOK_QUOTES.length)]
-        content_tag :div do 
-            content_tag :p, "'#{quote[:quote]}' by #{quote[:author]}", class:"quote"
-        end
-    end
 
     
 end
