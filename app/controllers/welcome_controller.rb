c9ac09f6f1eb7b921d74e3fc9720a9644eaa5c4c
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-08-08 00:02:41 -0500

    add welcom_helper that will randomly output quotes about books on welcome/home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
     def home
+        
     end
     def about
     end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,37 @@
 module WelcomeHelper
+    BOOK_QUOTES = [
+        {
+            quote:"If you don’t like to read, you haven’t found the right book." ,
+            author: "J.K.Rowling",
+        },
+        {
+            quote: "I love the smell of book ink in the morning." ,
+            author: "Umberto Eco",
+        },
+        {
+            quote: "The America I love still exists at the front desks of our public libraries.",
+            author: "Kurt Vonnequt",
+        },
+        {
+            quote: "Reading brings us unknown friends." ,
+            author: "Honore de Balzac",
+        },
+        {
+            quote: "A room without books is like a body without a soul.",
+            author: "Marcus Tullius Cicero",
+        },
+        {
+            quote: "There is no friend as loyal as a book" ,
+            author: "Ernest Hemingway",
+        }
+    ]
+
+    def book_quote
+        quote = BOOK_QUOTES[rand(1...BOOK_QUOTES.length)]
+        content_tag :div do 
+            content_tag :p, "'#{quote[:quote]}' by #{quote[:author]}", class:"quote"
+        end
+    end
+
+    
 end
