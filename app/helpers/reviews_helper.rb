1701bd27583af656ccb8a56069f07065556a77f9
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-08-11 14:03:04 -0500

    fix bugs in 'reviews_helper'

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -3,6 +3,12 @@ module ReviewsHelper
     #comb of one book and one user there can be only onew review
     #this checks it and returns [] or ["something here"]
     def one_review_per_user_per_book(book)
-        book.reviews.map{|review| review.user_id == current_user.id }
+        review_arr = []
+        book.reviews.each do |review| 
+            if review.user_id == current_user.id 
+                review_arr << review
+            end
+        end
+        review_arr
     end
 end
