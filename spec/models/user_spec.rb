81180c50004a3b46749fa61954db19bf01c83d2f
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-09 09:35:38 -0500

    add User attribute testing and User instance methods testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
+    describe User do
+        before(:each) do
+            @user = User.create!(first_name: "first name", last_name: 'last name', email:'example@user.com', password: '1234', birth_date: "01-01-2001", uid: "1234", provider: "google")
+        end
+        it "User can be created" do
+            expect(@user).to be_valid
+        end
+        it "User has instance method " do
+            expect(@user.full_name).to eq("first name last name")
+        end
+        it "User has instance method" do
+            expect(@user.member_since).to eq(@user.created_at.strftime("%B %d, %Y"))  
+        end
+        it "User has instance method 'format date' " do
+            expect(@user.format_date).to eq("January 01, 2001")
+        end
+        
+        
+    end
+    
     context "validation test" do
         it "ensure first name presence " do
             user = User.new(last_name: 'last name', email:'example@user.com', password: '1234').save
@@ -21,7 +41,7 @@ RSpec.describe User, :type => :model do
         end
     end
 
-    
+
     
     
     
